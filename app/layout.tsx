eca39efe13587e04648ca44c83cd5e2442c68a9d
Gourav Modi <gouravmodi1195@gmail.com>
2024-10-28 18:47:23 +0530

    auth-updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Inter as FontSans } from "next/font/google"
 import { cn } from "@/lib/utils"
 import './globals.css'
 import { Metadata } from "next"
-// import { ClerkProvider } from "@clerk/nextjs"
-// import { dark } from "@clerk/themes"
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
 // import Provider from "./Provider"
 
 const fontSans = FontSans({
@@ -19,15 +19,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    // <ClerkProvider
-    //   appearance={{
-    //     baseTheme: dark,
-    //     variables: { 
-    //       colorPrimary: "#3371FF" ,
-    //       fontSize: '16px'
-    //     },
-    //   }}
-    // >
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: { 
+          colorPrimary: "#3371FF" ,
+          fontSize: '16px'
+        },
+      }}
+    >
       <html lang="en" suppressHydrationWarning>
         <body
           className={cn(
@@ -38,6 +38,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           {children}
         </body>
       </html>
-    // </ClerkProvider> 
+     </ClerkProvider> 
   )
 }
